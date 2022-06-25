50f96070d923d0ced484839511a12d4138a7aced
Vinh NT <vinh.nguyen@thuocsi.vn>
2022-06-25 15:28:51 +0700

    public useCopyClipboard

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vdzblog.xyz/docs/"
           target="_blank"
           rel="noopener noreferrer"
           style={{ textDecoration: "none" }}
