8c64c2364e2e6773b7bf58d5353da4331882b3f6
Vinh NT <vinh.nguyen@thuocsi.vn>
2022-06-25 15:15:23 +0700

    public useClickOutSide

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ color: "#fa6f9d" }}>
+          Hello guy, your welcome. Apply it for your project now! 💕
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
         >
-          Learn React
+          Go to documentation ➜
         </a>
       </header>
     </div>
