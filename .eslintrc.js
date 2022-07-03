4d891c62ffbd33a544ffb1579d41d4c143247d69
Gearon__ <48727974+devntv@users.noreply.github.com>
2022-07-03 17:26:44 +0700

    fix duplicate parserOptions (#12)
    * fix issue module es

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,31 @@
-export default {
+module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   plugins: ["node", "unicorn", "prettier", "@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:node/recommended",
+    "plugin:react/recommended",
     "plugin:unicorn/recommended",
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/warnings",
+    "plugin:import/errors",
     "prettier",
   ],
   env: {
     node: true,
+    es6: true,
+    browser: true,
+    commonjs: true,
   },
   rules: {
     "no-console": "error",
